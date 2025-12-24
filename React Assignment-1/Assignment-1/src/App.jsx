c6bab9bf378e6d368bc461e062064d7de7e6a75b
Sabeel1786 <sabeeltanveer@gmail.com>
2025-12-24 18:40:12 +0530

    Validation

---
+++ React Assignment-1/Assignment-1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Validation from "./Components/Validation"
+
+function App() {
+
+  return (
+    <>
+    <Validation/>
+      
+    </>
+  )
+}
+
+export default App
